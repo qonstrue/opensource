a71d91b61ba633598c1b435010c6a8c2ce30d450
sustentabilitas <raj@sustentabilitas.com>
2025-05-26 11:59:06 +0100

    Add vitest to ts-doc-convert

---
+++ packages/ts-doc-convert/vite.config.ts
@@ -0,0 +1,20 @@
+/// <reference types='vitest' />
+import { defineConfig } from 'vite';
+
+export default defineConfig(() => ({
+  root: __dirname,
+  cacheDir: '../../node_modules/.vite/packages/ts-doc-convert',
+  plugins: [],
+  // Uncomment this if you are using workers.
+  // worker: {
+  //  plugins: [ nxViteTsPaths() ],
+  // },
+  test: {
+    watch: false,
+    globals: true,
+    environment: 'node',
+    include: ['{src,tests}/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+    reporters: ['verbose', 'junit'],
+    passWithNoTests: true,
+  },
+}));
